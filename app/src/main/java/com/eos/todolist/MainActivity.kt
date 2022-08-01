e62fa9a614d64167194944bcadf709df52f73721
jjonghyeon <jangapple@hanyang.ac.kr>
2022-08-01 18:00:25 +0900

    first commit

---
+++ app/src/main/java/com/eos/todolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.eos.todolist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eos.todolist.databinding.ActivityMainBinding
+import javax.net.ssl.SSLSessionBindingEvent
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, AddToDoActivity::class.java)
+            startActivity(intent)
+        }
+        setContentView(R.layout.activity_main)
+    }
+}
